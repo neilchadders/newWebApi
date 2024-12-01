68d8291205c360e89b59ea1385352e1e289a5346
Neil Chadwick <neilchadders@hotmail.com>
2024-12-01 12:09:32 +0000

    add UserRepo and IUserRepo

--- obj/Debug/net9.0/newWebAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/newWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf4cd4ada099e530816d434e0c9105d3255ad7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f277c487a9b114c5b6410c7fc212029cecb44c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("newWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
