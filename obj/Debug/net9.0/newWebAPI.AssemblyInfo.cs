a7f94d104cc1da8a617fa5d330309b0c09dcb9cf
Neil Chadwick <neilchadders@hotmail.com>
2024-12-03 18:44:23 +0000

    add post search

--- obj/Debug/net9.0/newWebAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/newWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8664994a54beb5cbab7978684a829a6886f965a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af6954435f7ab41cb2946f3c9b120ad7520c3ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("newWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
